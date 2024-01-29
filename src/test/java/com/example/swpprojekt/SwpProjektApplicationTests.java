abca790431424ce04bf598ff920f7a3f7994b929
oliverl2402 <oliver.liedl.student@htl-hallein.at>
2024-01-29 16:24:04 +0100

    g
    tdzhgchjv

---
+++ src/test/java/com/example/swpprojekt/SwpProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swpprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
